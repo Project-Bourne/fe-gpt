29d4c832a2139f9b78f022a39215d198a26ff0d5
code-sensei <tsowababangida@gmail.com>
2025-02-26 17:16:31 +0100

    fix: custom toast

--- src/services/notification.service.tsx
+++ src/services/notification.service.tsx
@@ -22,7 +22,7 @@ class NotificationService {
                 message={message} 
                 addedText={addedText} 
                 position={position}
-                />,
+            /> as any,
             container
         );
 
